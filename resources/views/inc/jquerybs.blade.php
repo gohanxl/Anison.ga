27104a792c532579546ab649b3b575a83c009361
Deterio <deterimo@gmail.com>
2018-07-26 20:15:12 +0200

    Change to cluster 1 of WidgetBot

--- resources/views/inc/jquerybs.blade.php
+++ resources/views/inc/jquerybs.blade.php
@@ -2,4 +2,4 @@
 <link href="{{url('/')}}/css/app.css" rel="stylesheet">
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-83961889-2"></script>
 <script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-83961889-2');</script>
-<script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async> const crate = new Crate({ server: '464077893350195201', channel: '464078826129981441' }) </script>
+<script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async> new Crate({ server: '464077893350195201', channel: '464078826129981441', shard: 'https://cl1.widgetbot.io' })</script>
